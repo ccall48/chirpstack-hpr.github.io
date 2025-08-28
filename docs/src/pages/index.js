e246a41193ce9a71ffad18ae872416d6919ae278
Bones <cory@callref.com.au>
2025-08-28 13:55:42 +1000

    update to landing page

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Chirpstack-HPR Tutorial ~ 60min ⏱️
           </Link>
         </div>
       </div>
